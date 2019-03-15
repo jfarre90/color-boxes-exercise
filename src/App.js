4432405df3c7b065ea54de32999f1d538bde2e00
Javier Farre <jfarre90@gmail.com>
2019-03-15 14:27:21 +0000

    exercise resolved

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const NUM_BOXES=32;
+
+const Box = ({color}) => {
+  const style = {
+    width: '180px',
+    height: '180px',
+    display: 'inline-block',
+    backgroundColor: color
+  } ; 
+  
+  return <div style={style} />;
+};
 class App extends Component {
+  
   
   constructor(props) {
     super(props);
+    const boxes = Array(NUM_BOXES).fill().map(this.getRandomColor, this); //The second argument determines what we want to take the value of this. We added this which actually refers to the APP component.
+    this.state = {boxes};
+    
+    setInterval(() => {
+      const boxes = this.state.boxes.slice();
+      const randIndex = Math.floor(Math.random()*boxes.length);
+      boxes[randIndex] = this.gerRandomColor();
+      this.setState({boxes});
+      
+    }, 300);
+    
+  }
+  
+  
+  
+  getRandomColor(){
+    let colorIndex = Math.floor(Math.random()*this.props.allColors.length);
+    return this.props.allColors[colorIndex];
   }
   
   render() {
-
+    const boxes = this.state.boxes.map((color,index) => (
+      <Box key={index} color={color} />
+    ));
     return (
       <div className="App">
-        Render boxes here
+        {boxes}
       </div>
     );
   }
